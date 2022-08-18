7e04ce4ab266c143d18f69deaa99c5d420a292fd
Tsuyoshi Tomozawa <sphdm244@gmail.com>
2022-08-18 09:13:32 +0900

    feat: 不要ユーザー削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,6 @@ User.create!(name: "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
-User.create!(name: "michael",
-             email: "sphdm244@gmail.com",
-             password: "banban244",
-             password_confirmation: "banban244",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
-
 99.times do |n|
     name = Faker::Name.name
     email = "example-#{n + 1}@railstutorial.org"
